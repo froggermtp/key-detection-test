e807d0fdd3b47415b38acede9bc8c9113746e876
Matthew Parris <mfrogparris@gmail.com>
2020-03-03 12:52:30 -0600

    Still testing image processing
    - Everything is a work in process

--- key-detection-test.py
+++ key-detection-test.py
@@ -5,14 +5,16 @@ import heapq
 from collections import namedtuple
 from pathlib import Path
 import argparse
+import math
 
 
-Example = namedtuple('Example', 'filename threshold')
+Example = namedtuple('Example', 'filename threshold features')
 
 
 def setup_cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--view_thresholds', action="store_true")
+    parser.add_argument('-t', '--view_thresholds', action="store_true")
+    parser.add_argument('-v', '--video', action="store_true")
     parser.add_argument('-i', '--ip', default='127.0.0.1')
     return parser.parse_args()
 
@@ -21,10 +23,11 @@ def view_all_examples(examples):
     for example in examples:
         view_example(example.filename, example.threshold)
 
+    cv2.waitKey(0)
+
 
 def view_example(filename, example):
     cv2.imshow(filename, example)
-    cv2.waitKey(0)
 
 
 def load_key(filename):
@@ -34,35 +37,71 @@ def load_key(filename):
 
 def process_image(filename, im):
     resized = imutils.resize(im, 250)
-    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-
-    # I'm messing around with different bluring techniques.
-    # Don't know which is better. Most people use Gaussian.
-    # The blur step reduces noise. I.E. makes the contours sharper.
-
-    # im = cv2.medianBlur(im, 3)
-    #blurred = cv2.bilateralFilter(im, 5, 75, 75)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    sobled = sobel(blurred)
-    final_im = reduce_noise(sobled)
-
+    im_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    edges = canny_edge_detection(im_gray)
+    final_im = reduce_noise(edges)
     contours = findContours(final_im)
-    blank_image = np.zeros((600, 600, 3), np.uint8)
+    features = extract_features(im_gray, resized, contours)
+
+    blank_image = np.zeros(resized.shape, np.uint8)
     cv2.drawContours(blank_image, contours, -1, (0, 255, 0), 3)
 
-    # I hope that the contours for each key is unique enough to do the matching.
-    # The contours can be plugged into the matchShape function. (Further down)
-    # As you will see, the key on the wood background is not working well.
-    # I don't know how to fix that. (Can we fix it???)
+    return Example(filename, blank_image, features)
 
-    return Example(filename, blank_image)
+
+def extract_features(gray_im, im, contours):
+    mask = compute_mask(gray_im, contours)
+    mean_val = compute_mean_contour_value(im, mask)
+    aspect_ratio = compute_aspect_ratio(contours)
+    extent = compute_extent(contours)
+    return [*mean_val, aspect_ratio, extent]
+
+
+def compute_mask(gray_im, contours):
+    mask = np.zeros(gray_im.shape, np.uint8)
+    cv2.drawContours(mask, [contours], 0, 255, -1)
+    return mask
+
+
+def compute_mean_contour_value(im, mask):
+    mean_val = cv2.mean(im, mask=mask)
+    return mean_val
+
+
+def compute_aspect_ratio(contours):
+    x, y, w, h = cv2.boundingRect(contours)
+    aspect_ratio = float(w) / h
+    return aspect_ratio
+
+
+def compute_extent(contours):
+    area = cv2.contourArea(contours)
+    x, y, w, h = cv2.boundingRect(contours)
+    rect_area = w * h
+    extent = float(area) / rect_area
+    return extent
+
+
+def white_balance(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - \
+        ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - \
+        ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
+
+
+def canny_edge_detection(im_gray):
+    im_gray_blurred = cv2.GaussianBlur(im_gray, (5, 5), 0)
+    edges = cv2.Canny(im_gray, 0, 100)
+
+    return edges
 
 
 def sobel(im):
-    # This is an algorithm for doing edge detection.
-    # Someone should probably look into Canny Edge detection.
-    # It's supposedly better...
-
     scale = 1
     delta = 0
     ddepth = cv2.CV_16S
@@ -87,29 +126,15 @@ def reduce_noise(im):
     return im
 
 
-def findContours(im):
-    # Returns the largest contour from the image.
-    # im -> do edge detection first before finding contours
-
+def findContours(edges):
     contours, hierarchy = cv2.findContours(
-        im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # This is very naive. Will probably have to be changed.
     # Simply finds the largest contour. The key is unlikely to always be the largest contour.
     c = max(contours, key=cv2.contourArea)
 
-    return c
-
-
-def process_image2(filename, im):
-    # Not currently being used.
-    # I think I was messing with something. Don't remember.
-
-    img_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(img_gray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, 2, 1)
-
-    return Example(filename, contours[0])
+    return contours
 
 
 def setup_training_set():
@@ -119,13 +144,39 @@ def setup_training_set():
     return examples
 
 
+def setup_test_set():
+    filenames_for_testing = map(str, Path('./test-set').glob('*.jpg'))
+    examples = [load_key(filename) for filename in filenames_for_testing]
+
+    return examples
+
+
+def run_trials(training_set, test_set):
+    for img in test_set:
+        classification = which_key(img, training_set)
+        msg = 'Classify {0} as {1}'.format(img.filename, classification)
+        print(msg)
+
+
 def which_key(key, training_set):
     distances = []
 
     for example in training_set:
+        # print(example.filename)
+        # print(key.features)
+        # print(example.features)
         d2 = cv2.matchShapes(
             key.threshold, example.threshold, cv2.CONTOURS_MATCH_I2, 0)
-        heapq.heappush(distances, (d2, example.filename))
+
+        distance = 0
+        for ii in range(len(example.features)):
+            term1 = key.features[ii] - example.features[ii]
+            term2 = term1**2
+            distance += term2
+
+        distance = math.sqrt(distance) + d2
+
+        heapq.heappush(distances, (distance, example.filename))
 
     print(distances)
 
@@ -165,5 +216,9 @@ if __name__ == '__main__':
     if args.view_thresholds:
         view_all_examples(training_set)
 
-    # I have the video stuff disabled for now
-    # open_camera(training_set, args.ip)
+    if not args.video:
+        test_set = setup_test_set()
+        view_all_examples(test_set)
+        run_trials(training_set, test_set)
+    else:
+        open_camera(training_set, args.ip)
